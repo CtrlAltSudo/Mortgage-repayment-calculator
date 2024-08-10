0648a06dc477c27b9def57085a0d8529078a705a
CtrlAltSudo <damienkwhite@gmail.com>
2024-08-10 21:42:30 +0100

    added some lines of js

--- app.js
+++ app.js
@@ -3,3 +3,8 @@ const inputs = document.querySelectorAll('input');
 const amount = inputs[0].value;
 const term = inputs[1].value;
 const interest = inputs[2].value;
+
+const mortgageType = document.querySelectorAll('.checkmark');
+const repaymentCheck = mortgageType[0]
+const interestCheck = mortgageType[1]
+
