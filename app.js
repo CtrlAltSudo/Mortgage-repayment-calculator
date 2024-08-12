fada538ae2e8b3fbc18da28ea853a761d97e65d8
CtrlAltSudo <damienkwhite@gmail.com>
2024-08-12 21:53:38 +0100

    attempting to write a function for calculation

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const inputs = document.querySelectorAll('input');
 
-const amount = inputs[0].value;
-const term = inputs[1].value;
-const interest = inputs[2].value;
+const amount = inputs[0].valueAsNumber;
+const term = inputs[1].valueAsNumber;
+const interest = inputs[2].valueAsNumber;
 
 const mortgageType = document.querySelectorAll('.checkmark');
 const repaymentCheck = mortgageType[0]
 const interestCheck = mortgageType[1]
 
+
+function calculate(amount, term, interest) {
+    const annualInterestRate = interest / 100;
+    const totalInterest = amount * annualInterestRate * term;
+    const totalAmount = amount + totalInterest;
+    console.log(`Total repayment after ${term} years: ${totalAmount}`);
+}
